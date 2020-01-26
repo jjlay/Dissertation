f9846bacddf09e46dffaa073bc13fbb8ddd36b4d
JJ Lay <JJ.Lay@tmcat.com>
2020-01-25 23:45:16 -0600

    Did nto realize I had two different test scripts! Merged the two versions into the appendix solution.

--- Appendix/WelfordTest/WelfordTest.cpp
+++ Appendix/WelfordTest/WelfordTest.cpp
@@ -1,9 +1,12 @@
-
-
 /*
  * Welford online variance calculator
  *
  * See also
+ *
+ * Welford, B. P. (1962). "Note on a method for calculating
+ * corrected sums of squares and products".
+ * Technometrics. 4 (3), pp. 419–420.
+ *
  * Lay, H., Colgin, Z., Reshniak, V., Khaliq, A.Q.M. (2018).
  * "On the implementation of multilevel Monte Carlo simulation
  * of the stochastic volatility and interest rate model using
@@ -19,14 +22,13 @@
  * 2018-12-16  JJL     Initial version
  *
  */
-
-
+ 
+ 
 //
 // Local includes
 //
 
 #include "Welford.h"
-#include "ReturnValues.h"
 
 
 //
@@ -34,7 +36,6 @@
 //
 
 #include <random>
-#include <math.h>
 #include <iostream>
 #include <iomanip>
 
@@ -42,66 +43,104 @@
 //
 // Function: main()
 //
-// Parameters:
-//
-// Returns:
-//
 
 int main(int argc, char* argv[]) {
 
+	//////////////////////
 	//
-	// Values for checking the Welford algorithm
-	//
-
-	const double testMean = 0.0;
-	const double testVariance = 10.0;
-	const unsigned int testSamples = static_cast<unsigned int>(1E8);
-
-
-	//
-	// Required Welford temp variables
+	// Welford parameters
 	//
 
-	double count = 0.0, mean = 0.0, M2 = 0.0;
+	//	void welford(double* pcount, double* pmean, double* pM2, double pNewValue);
+	//	double welfordVariance(double* pCount, double* pMean, double* pM2);
 
+	double count = 0.0, mean = 0.0, M2 = 0.0, x = 0.0;
+	int samples = 1000000;
+
+	auto storage = new double[samples];
+
+	for (auto i = 0; i < samples; i++)
+		storage[i] = 0.0;
+
+
+	//////////////////////
 	//
-	// Random number generator
+	// Random parameters
 	//
 
+	double randMean = 2.0, randStdDev = 3.0;
 	std::default_random_engine generator;
-	std::normal_distribution<double> normal_distribution(testMean, sqrt(testVariance));
+	std::normal_distribution<double> normal(randMean, randStdDev);
 
+
+	//////////////////////
 	//
-	// Perform test
+	// Collect the data
 	//
 
-	for (auto sample = 0; sample < testSamples; sample++) {
-		auto x = normal_distribution(generator);
+	for (auto i = 0; i < samples; i++) {
+		// Take our random number
+		x = normal(generator);
 
+		storage[i] = x;
 		welford(&count, &mean, &M2, x);
 	}
 
-	auto var = welfordVariance(&count, &mean, &M2);
-
-	std::cout << "Welford results" << std::endl
-		<< "Test values:" << std::endl
-		<< "mean = " << std::fixed << std::setprecision(4) << mean << std::endl
-		<< "variance = " << std::fixed << std::setprecision(4) << testVariance << std::endl
-		<< "samples = " << std::scientific << std::setprecision(1) << static_cast<double>(testSamples) << std::endl
-		<< std::endl
-		<< "Results:" << std::endl
-		<< "mean = " << std::fixed << std::setprecision(4) << mean << std::endl
-		<< "variance = " << std::fixed << std::setprecision(4) << var << std::endl
-		<< std::endl
-		<< "Error:" << std::endl
-		<< "mean = " << std::scientific << std::setprecision(2) << mean - testMean << std::endl
-		<< "variance = " << std::scientific << std::setprecision(2) << var - testVariance << std::endl
-		<< std::endl;
-
-
+	//////////////////////
 	//
-	// Wrap up
+	// Calculate the statistics with two passes
 	//
 
-	return _OKAY_;
+	double pass2Mean = 0.0, pass2Variance = 0.0;
+
+	// Mean
+
+	for (auto i = 0; i < samples; i++) {
+		pass2Mean += storage[i];
+	}
+
+	pass2Mean = pass2Mean / static_cast<double>(samples);
+
+	// Variance
+
+	for (auto i = 0; i < samples; i++) {
+		pass2Variance += pow(storage[i] - pass2Mean, 2.0);
+	}
+
+	pass2Variance = pass2Variance / static_cast<double>(samples - 1);
+
+	
+	//////////////////////
+	//
+	// Calculate resulrs from Welford
+	//
+
+	auto wv = welfordVariance(&count, &mean, &M2);
+
+
+	//////////////////////
+	//
+	// Results
+	//
+
+	std::cout << "Actual values:" << std::endl
+		<< "   Mean = " << std::fixed << std::setprecision(5) << randMean << std::endl
+		<< "   Std Dev = " << randStdDev << std::endl
+		<< "   Number of Samples = " << samples << std::endl << std::endl
+
+		<< "Two-pass estimation:" << std::endl
+		<< "   Mean = " << std::fixed << std::setprecision(5) << pass2Mean
+		<< ", Error = " << std::scientific << std::setprecision(3) << (pass2Mean - randMean) << std::endl
+		<< "   Std Dev = " << std::fixed << std::setprecision(5) << sqrt(pass2Variance)
+		<< ", Error = " << std::scientific << std::setprecision(3) << (sqrt(pass2Variance) - randStdDev)
+		<< std::endl << std::endl
+
+		<< "Welford estimation:" << std::endl
+		<< "   Mean = " << std::fixed << std::setprecision(5) << mean
+		<< ", Error = " << std::scientific << std::setprecision(3) << (mean - randMean) << std::endl
+		<< "   Std Dev = " << std::fixed << std::setprecision(5) << sqrt(wv)
+		<< ", Error = " << std::scientific << std::setprecision(3) << (sqrt(wv) - randStdDev)
+		<< std::endl << std::endl;
+
+	return 0;
 }
